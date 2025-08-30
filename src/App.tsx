55f7ff0017a8e55c725980a8a2afa397d77f7b4d
rxssula <rxssul.aitkali@gmail.com>
2025-08-30 14:44:55 +0500

    feat: render basic google maps api with split layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { SplitLayout } from "@googlemaps/extended-component-library/react";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
+
+const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY as string;
+
 function App() {
-  return <div className="text-2xl">Hello</div>;
+  return (
+    <div className="h-screen">
+      <APIProvider apiKey={GOOGLE_MAPS_API_KEY}>
+        <SplitLayout rowLayoutMinWidth={700}>
+          <div className="text-2xl h-full" slot="fixed">
+            Hello
+          </div>
+          <div className="h-full" slot="main">
+            <Map></Map>
+          </div>
+        </SplitLayout>
+      </APIProvider>
+    </div>
+  );
 }
 
 export default App;
